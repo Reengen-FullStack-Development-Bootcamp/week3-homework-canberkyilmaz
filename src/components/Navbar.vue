500e257ec2080552089b4e3bdbb44f53c1f0750c
Canberk-yilmaz <cyilmaz089@gmail.com>
2021-11-05 18:08:06 +0300

    component: Navbar add

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav>
+    <v-app-bar app text color="indigo" dark>
+      <v-toolbar-title
+        class="text-uppercase grey--text text--lighten-2 d-none d-sm-flex"
+      >
+        <span class="font-weight-light">Canberk</span>
+        <span class="font-weight-bold">Yılmaz</span>
+      </v-toolbar-title>
+      <v-spacer class="d-none d-sm-flex"></v-spacer>
+      <v-toolbar-items>
+        <v-btn text to="/" class="text-uppercase grey--text text--lighten-4">
+          HOME
+        </v-btn>
+      </v-toolbar-items>
+      <v-toolbar-items>
+        <v-btn
+          text
+          to="/about"
+          class="text-uppercase grey--text text--lighten-4"
+        >
+          ABOUT
+        </v-btn>
+      </v-toolbar-items>
+      <v-toolbar-items>
+        <v-btn
+          text
+          to="/search"
+          class="text-uppercase grey--text text--lighten-4"
+        >
+          SEARCH
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
