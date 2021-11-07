283f654a2ce2b5f535993e4efec6e70c84d677d4
canberk-yilmaz <cyilmaz089@gmail.com>
2021-11-08 01:26:46 +0300

    feat: hamburger menu add for mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,23 @@
 <template>
   <nav>
     <v-app-bar app text color="indigo" dark>
-      <v-toolbar-title
-        class="text-uppercase grey--text text--lighten-2 d-none d-sm-flex"
-      >
+      <v-toolbar-title class="text-uppercase grey--text text--lighten-2">
         <span class="font-weight-light">Canberk</span>
         <span class="font-weight-bold">Yılmaz</span>
       </v-toolbar-title>
+      <v-layout justify-end class="d-flex d-sm-none">
+        <v-app-bar-nav-icon
+          @click="drawer = true"
+          class="d-flex d-sm-none"
+        ></v-app-bar-nav-icon>
+      </v-layout>
       <v-spacer class="d-none d-sm-flex"></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items class="d-none d-sm-flex">
         <v-btn text to="/" class="text-uppercase grey--text text--lighten-4">
           HOME
         </v-btn>
       </v-toolbar-items>
-      <v-toolbar-items>
+      <v-toolbar-items class="d-none d-sm-flex">
         <v-btn
           text
           to="/about"
@@ -22,7 +26,7 @@
           ABOUT
         </v-btn>
       </v-toolbar-items>
-      <v-toolbar-items>
+      <!-- <v-toolbar-items>
         <v-btn
           text
           to="/search"
@@ -30,13 +34,29 @@
         >
           SEARCH
         </v-btn>
-      </v-toolbar-items>
+      </v-toolbar-items> -->
     </v-app-bar>
+    <!-- Add a navigation bar -->
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item to="/"> HOME </v-list-item>
+          <v-list-item to="/about"> ABOUT </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- Navigation bar ends -->
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
 </script>
 
 <style scoped></style>
