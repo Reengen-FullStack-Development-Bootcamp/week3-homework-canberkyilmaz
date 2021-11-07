67b9bd3424910ba7a2e4235f0b66e907a8bb3381
canberk-yilmaz <cyilmaz089@gmail.com>
2021-11-08 01:28:13 +0300

    feat: ticker and stock data api function complete

--- src/store/index.js
+++ src/store/index.js
@@ -8,68 +8,54 @@ export default new Vuex.Store({
   state: {
     title: "patika",
     number: 0,
-    apiURL: "https://alpha-vantage.p.rapidapi.com/query",
-    headers: {
-      "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
-      "x-rapidapi-key": process.env.VUE_APP_RAPID_API_KEY,
-    },
-    dailySeries: "TIME_SERIES_DAILY",
-    symbol: "MSFT",
-    dailyData: {},
+    apiURL: "https://www.alphavantage.co/query",
+    apikey: process.env.VUE_APP_API_KEY,
+    headers: { "User-Agent": "Canberk LLC" },
+    stockData: {},
+    symbolsFound: [],
   },
   mutations: {
-    SET_DAILY_RESULT(state, payload) {
-      state.dailyData = payload;
+    SET_RESULT(state, payload) {
+      state.stockData = payload;
+    },
+    SET_SYMBOLS_FOUND(state, payload) {
+      state.symbolsFound = payload;
     },
   },
   actions: {
-    searchStockDaily({ state, commit }, payload) {
+    searchSymbol({ state, commit }, payload) {
       axios
         .get(`${state.apiURL}`, {
           headers: { ...state.headers },
-          params: { function: state.dailySeries, symbol: payload },
+          params: {
+            function: "SYMBOL_SEARCH",
+            keywords: payload,
+            apikey: state.apikey,
+          },
         })
         .then((res) => {
           console.log(res);
-          commit("SET_DAILY_RESULT", res.data);
+          commit("SET_SYMBOLS_FOUND", res.data.bestMatches);
         })
         .catch((err) => console.error(err));
     },
-    // getNumber({ commit }, payload) {
-    //   let number = 0;
-    //   for (let i = 0; i <= payload; i++) {
-    //     number += i;
-    //   }
-    //   commit("SET_NUMBER", number);
-    // },
-    // searchFromMovieTitle({ state, commit }, payload) {
-    //   axios
-    //     .get(`${state.apiURL}title/find`, {
-    //       headers: { ...state.headers },
-    //       params: { q: payload },
-    //     })
-    //     .then((res) => {
-    //       console.log(res.data.results);
-    //       let data = [];
-    //       for (let i = 0; i < res.data.results.length; i++) {
-    //         if (res.data.results[i].image) data.push(res.data.results[i]);
-    //       }
-    //       commit("SET_MOVIE_SEARCH_RESULT", data);
-    //     })
-    //     .catch((err) => console.log(err));
-    // },
+    searchStockSeries({ state, commit }, payload) {
+      axios
+        .get(`${state.apiURL}`, {
+          headers: { ...state.headers },
+          params: {
+            function: `TIME_SERIES_${payload.selectedTimeSeries}`,
+            apikey: state.apikey,
+            symbol: payload.selectedTicker,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          commit("SET_RESULT", res.data);
+        })
+        .catch((err) => console.error(err));
+    },
   },
-  getters: {
-    // searchTypeMovie: (state) => {
-    //   let res = 0;
-    //   for (let i = 0; i < state.searchResults.length; i++) {
-    //     if (state.searchResults[i].titleType === "movie") res++;
-    //   }
-    //   return res;
-    // },
-    // favNumber: (state) => {
-    //   return state.favMovies.length;
-    // },
-  },
+  getters: {},
   modules: {},
 });
