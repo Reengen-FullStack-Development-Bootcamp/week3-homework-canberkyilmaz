12d1cef5ef4d8cb121ae2eff8be9e96c558387fb
canberk-yilmaz <cyilmaz089@gmail.com>
2021-11-08 01:28:53 +0300

    feat: ticker and frequency select add

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,44 +1,152 @@
 <template>
-  <v-container>
-    <v-row class="text-center justify-center">
-      <v-col cols="6">
-        {{ title }}
+  <v-app>
+    <v-container>
+      <v-card class="mt-5" xs-12 lg-4 color="indigo lighten-2" dark>
+        <v-card-title class="text-h5 indigo lighten-1">
+          Search Stock Symbol
+        </v-card-title>
+        <v-card-text> Choose stock data to sea the charts. </v-card-text>
+        <v-card-text>
+          <v-form class="px-3">
+            <v-text-field
+              prepend-icon="mdi-database-search"
+              @keydown.enter.prevent="searchTicker"
+              label="Search Here"
+              v-model="searchValue"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
 
-        <v-text-field
-          class="text-align"
-          v-model="value"
-          type="number"
-          @keydown.enter.prevent="getNumSum"
-        >
-        </v-text-field>
+        <v-expand-transition>
+          <v-list v-if="symbols.length > 0" class="indigo lighten-3">
+            <v-list-item
+              v-for="(symbol, i) in symbols"
+              :key="symbol.value['1. symbol']"
+              @click="setTicker(symbol.value['1. symbol'])"
+              :ref="i"
+            >
+              <v-list-item-avatar
+                color="indigo lighten-2"
+                class="text-h4 font-weight-light"
+              >
+                {{ symbol.value["1. symbol"].charAt(0) }}
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title
+                  v-text="symbol.value['2. name']"
+                ></v-list-item-title>
+                <v-list-item-subtitle
+                  v-text="symbol.value['1. symbol']"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-expand-transition>
 
-        <v-btn @click="getNumSum">Get Sum</v-btn>
-        <div>Toplam: {{ calNumber }}</div>
-      </v-col>
-    </v-row>
-  </v-container>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="indigo darken-1" @click="searchTicker">
+            Search
+            <v-icon right color="white"> mdi-magnify </v-icon>
+          </v-btn>
+          <v-btn
+            :disabled="!searchValue"
+            color="indigo darken-1"
+            @click="searchValue = ''"
+          >
+            Clear
+            <v-icon right> mdi-close-circle </v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-row>
+        <v-col>
+          <h4>Selected Ticker: {{ selectedTicker }}</h4>
+          <h4>Selected Frequency: {{ selectedTimeSeries }}</h4>
+        </v-col>
+      </v-row>
+
+      <template>
+        <v-tabs color="indigo">
+          <v-tab v-for="item in items" :key="item" @click="setTimeSeries(item)"
+            >{{ item }} TIME SERIES</v-tab
+          >
+        </v-tabs>
+      </template>
+      <v-btn color="indigo darken-1" @click="setStockSeries">
+        GET ME THE CHART
+        <v-icon right color="white"> mdi-magnify </v-icon>
+      </v-btn>
+      <div v-if="this.selectedTicker && this.selectedTimeSeries">
+        <p>{{ this.$store.state.stockData }}</p>
+      </div>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
+
 export default {
-  name: "Home",
-  data() {
-    return {
-      value: 0,
-    };
-  },
+  data: () => ({
+    descriptionLimit: 60,
+    entries: [],
+    isLoading: false,
+    model: null,
+    searchValue: "",
+    search: "",
+    selectedTicker: null,
+    items: ["DAILY", "WEEKLY", "MONTHLY"],
+    selectedTimeSeries: "",
+  }),
+
   computed: {
-    title() {
-      return this.$store.state.title;
+    symbols() {
+      if (!this.symbolsFound) return [];
+
+      return Object.keys(this.symbolsFound).map((key) => {
+        return {
+          key,
+          value: this.symbolsFound[key] || "n/a",
+        };
+      });
     },
-    calNumber() {
-      return this.$store.state.number;
-    },
+
+    ...mapActions(["searchStockSeries"]),
+    ...mapState(["symbolsFound"]),
   },
   methods: {
-    getNumSum() {
-      this.$store.dispatch("getNumber", parseInt(this.value));
+    searchTicker() {
+      this.$store.dispatch("searchSymbol", this.searchValue);
+    },
+    setStockSeries() {
+      if (this.selectedTicker && this.selectedTimeSeries) {
+        this.$store.dispatch("searchStockSeries", {
+          selectedTicker: this.selectedTicker,
+          selectedTimeSeries: this.selectedTimeSeries,
+        });
+      }
+    },
+
+    setTicker(ticker) {
+      this.selectedTicker = ticker;
+      console.log(this.selectedTicker);
+    },
+    setTimeSeries(timeSeries) {
+      this.selectedTimeSeries = timeSeries;
+      console.log(this.selectedTimeSeries);
+    },
+  },
+  watch: {
+    searchValue() {
+      // to stay within api limits search will start after 3 characters.
+      if (this.searchValue.length === 0 || this.searchValue.length >= 3) {
+        this.searchTicker();
+      }
     },
   },
 };
 </script>
+
+<style scoped></style>
