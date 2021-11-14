61287580b5e6b1fd97a874ef8578f1189b6473b5
canberk-yilmaz <cyilmaz089@gmail.com>
2021-11-14 21:06:28 +0300

    fix: functions change for better response

--- src/store/index.js
+++ src/store/index.js
@@ -10,14 +10,17 @@ export default new Vuex.Store({
     number: 0,
     apiURL: "https://www.alphavantage.co/query",
     apikey: process.env.VUE_APP_API_KEY,
-    stockData: {},
+    stockDataSeries: {},
     symbolsFound: [],
-    userStatus: ["Admin", "Guest"],
+    userStatus: "Admin",
     loadingStatus: false,
+    logs: ["deneme1", "deneme2"],
+    selectedTicker: "",
+    selectedTimeSeries: "DAILY",
   },
   mutations: {
-    SET_RESULT(state, payload) {
-      state.stockData = payload;
+    SET_RESULT_DATA(state, payload) {
+      state.stockDataSeries = payload;
     },
     SET_SYMBOLS_FOUND(state, payload) {
       state.symbolsFound = payload;
@@ -28,8 +31,17 @@ export default new Vuex.Store({
     LOADING_STATUS(state, newLoadingStatus) {
       state.loadingStatus = newLoadingStatus;
     },
+    SET_TICKER(state, payload) {
+      state.selectedTicker = payload;
+    },
+    SET_TIME_SERIES(state, payload) {
+      state.selectedTimeSeries = payload;
+    },
   },
   actions: {
+    setTickerAction({ commit }, payload) {
+      commit("SET_TICKER", payload);
+    },
     setUser({ commit }, payload) {
       commit("SET_USER_STATUS", payload);
     },
@@ -46,74 +58,82 @@ export default new Vuex.Store({
           },
         })
         .then((res) => {
-          commit("SET_SYMBOLS_FOUND", res.data.bestMatches);
+          if (res.status === 200) {
+            if (res.data.Note) {
+              throw new Error(res.data.Note);
+            }
+            commit("SET_SYMBOLS_FOUND", res.data.bestMatches);
+          }
         })
         .catch((err) => console.error(err))
-        .finally(
-          () => (
-            commit("LOADING_STATUS", false), console.log(state.loadingStatus)
-          )
-        );
+        .finally(() => commit("LOADING_STATUS", false));
     },
     searchStockSeries({ state, commit }, payload) {
       commit("LOADING_STATUS", true);
-      console.log(state.loadingStatus);
       axios
         .get(`${state.apiURL}`, {
           params: {
-            function: `TIME_SERIES_${payload.selectedTimeSeries}`,
+            function: `${payload.selectedTimeSeries}`,
             apikey: state.apikey,
             symbol: payload.selectedTicker,
           },
         })
         .then((res) => {
-          commit("SET_RESULT", res.data);
+          if (res.status === 200) {
+            if (res.data.Note) {
+              throw new Error(res.data.Note);
+            }
+            commit(
+              "SET_RESULT_DATA",
+              res.data[`${this.getters.dataTimeSeriesFormat}`]
+            );
+          }
         })
         .catch((err) => console.error(err))
-        .finally(
-          () => (
-            commit("LOADING_STATUS", false), console.log(state.loadingStatus)
-          )
-        );
+        .finally(() => commit("LOADING_STATUS", false));
     },
   },
   getters: {
-    getFrequencyTag: (state) => (freq) => {
-      let frequencyTag = "";
-
-      freq = freq[0].toUpperCase() + freq.toLowerCase().slice(1, freq.length);
-
-      switch (freq) {
-        case "Daily":
-          frequencyTag = `Time Series (${freq})`;
-          break;
-
-        case "Weekly":
-          frequencyTag = `${freq} Time Series`;
-          break;
-
-        case "Monthly":
-          frequencyTag = `${freq} Time Series`;
-          break;
-
+    apiTimeSeriesFormat(state) {
+      switch (state.selectedTimeSeries) {
+        case "DAILY":
+          return "TIME_SERIES_DAILY";
+        case "WEEKLY":
+          return "TIME_SERIES_WEEKLY";
+        case "MONTHLY":
+          return "TIME_SERIES_MONTHLY";
         default:
           break;
       }
-
-      const timeSeriesData = state.stockData[frequencyTag];
-      if (timeSeriesData) {
-        return Object.keys(timeSeriesData).map((key) => {
-          return {
-            date: key,
-            open: parseFloat(timeSeriesData[key]["1. open"]),
-            high: parseFloat(timeSeriesData[key]["2. high"]),
-            low: parseFloat(timeSeriesData[key]["3. low"]),
-            close: parseFloat(timeSeriesData[key]["4. close"]),
-            volume: parseFloat(timeSeriesData[key]["5. volume"]),
-          };
-        });
+    },
+    dataTimeSeriesFormat(state) {
+      switch (state.selectedTimeSeries) {
+        case "DAILY":
+          return "Time Series (Daily)";
+        case "WEEKLY":
+          return "Weekly Time Series";
+        case "MONTHLY":
+          return "Monthly Time Series";
+        default:
+          break;
       }
     },
+    timeSeriesArray(state) {
+      return Object.keys(state.stockDataSeries).map((key) => {
+        return {
+          date: key,
+          open: parseFloat(state.stockDataSeries[key]["1. open"]),
+          high: parseFloat(state.stockDataSeries[key]["2. high"]),
+          low: parseFloat(state.stockDataSeries[key]["3. low"]),
+          close: parseFloat(state.stockDataSeries[key]["4. close"]),
+          volume: parseFloat(state.stockDataSeries[key]["5. volume"]),
+        };
+      });
+    },
+    getUserStatus(state) {
+      //if Admin true, else false
+      return state.userStatus === "Admin"
+    }
   },
   modules: {},
 });
