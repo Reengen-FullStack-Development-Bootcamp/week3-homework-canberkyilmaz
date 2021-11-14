7f32b430cf49f0513bd2b6b1505ae382a5e6140e
canberk-yilmaz <cyilmaz089@gmail.com>
2021-11-14 21:00:42 +0300

    feat: chart drawing children route view add

---
+++ src/views/StockChart.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>a</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    // Company symbol value at URL params.
+    const ticker = this.$route.params.symbol;
+    this.ticker = ticker;
+    // this.getCompanyDailyDetails({
+    //   symbol: symbol,
+    //   function: this.getApiFunctions().daily.function,
+    // });
+  },
+};
+</script>
+
+<style scoped></style>
