750e09357f16783c943102f47274de697b668274
canberk-yilmaz <cyilmaz089@gmail.com>
2021-11-14 23:52:39 +0300

    fix: unneccessary line delete

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,6 @@ export default new Vuex.Store({
     symbolsFound: [],
     userStatus: "Admin",
     loadingStatus: false,
-    logs: ["deneme1", "deneme2"],
     selectedTicker: "",
     selectedTimeSeries: "DAILY",
   },
